b23d3459e60f8cedbb85b391ae9bccdbc3e19f6b
Sorin <kidoatguitar@gmail.com>
2019-01-27 23:43:48 +0200

    first commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/postss')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            //distribute the props of the post
+            ...post,
+            // and add hardcodded author
+            author: 'Sorin'
+          }
+        })
+        this.setState({ posts: updatedPosts })
+      })
+      .catch((error) => {
+        // console.log('error', error)
+        this.setState({ error: true })
+      })
+  }
+
+  // id of post that was selected
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id })
+  }
+
+  render() {
+    let posts = <p>Something went wrong</p>
+
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)} />
+      });
+    }
+    return (
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
